60c2976e4f2f40de39a8ae2f46fa799845d92a84
Alif Rahmadian <alifrah78@yahoo.com>
2025-09-21 16:39:13 +0700

    feat(create order): add create order repository

--- src/internals/repository/order.go
+++ src/internals/repository/order.go
@@ -1,3 +1,32 @@
 package repository
 
+import (
+	"sync"
+
+	"github.com/audricimanuel/awb-stock-allocation/src/model"
+)
+
 // TODO: the interface and implementation under here
+type (
+	OrderRepository interface {
+		CreateOrder(order *model.Order) (*model.Order, error)
+	}
+
+	OrderRepositoryImpl struct {
+		mu   sync.RWMutex
+		list *[]model.Order
+	}
+)
+
+func NewOrderRepository(list *[]model.Order) OrderRepository {
+	return &OrderRepositoryImpl{
+		list: list,
+	}
+}
+
+func (r *OrderRepositoryImpl) CreateOrder(order *model.Order) (*model.Order, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	return nil, nil
+}
